f882b0e4785d36bef5d45c9b5d0715c159b06ab7
Aleksandr <aik5787@gmail.com>
2023-08-26 09:14:42 -0400

    task 1.3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,48 @@
-import { Feedback } from './Feddback/Feedback';
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
 
 
-export const App = () => {
-  return (
-    <>
-      <Feedback/>
-    </>
-  );
-};
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+    handleFeedbackClick = (type) => {
+    this.setState((prevState) => ({
+      [type]: prevState[type] + 1,
+    }));
+    };
+  
+  
+  
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positivePercentage = total > 0 ? Math.round((good / total) * 100) : 0;
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleFeedbackClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </>
+    );
+  }
+}
