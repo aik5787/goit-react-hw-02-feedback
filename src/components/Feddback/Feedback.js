3582f14ea6f0b841b21333715bef32760cad6309
Aleksandr <aik5787@gmail.com>
2023-08-26 08:17:16 -0400

    task 1.2

--- src/components/Feddback/Feedback.js
+++ src/components/Feddback/Feedback.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Container,Tittle, Button, FeedbackCounter } from './Feedback.styled'
+import { Container,Tittle, ButtonContainer, Button, StatisticsContainer, FeedbackCounter } from './Feedback.styled'
 
 
 export class Feedback extends Component {
@@ -18,20 +18,42 @@ export class Feedback extends Component {
      };
     
     
+     countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+     };
+    
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    return totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  };
+    
+    
      render() {
     const { good, neutral, bad } = this.state;
 
     return (
       <Container >
         <Tittle>Please leave feedback</Tittle>
-        <Button onClick={() => this.handleFeedbackClick('good')}>Good</Button>
+            <ButtonContainer>
+                <Button onClick={() => this.handleFeedbackClick('good')}>Good</Button>
         <Button onClick={() => this.handleFeedbackClick('neutral')}>Neutral</Button>
         <Button onClick={() => this.handleFeedbackClick('bad')}>Bad</Button>
+        </ButtonContainer>
 
-        <Tittle>Statistics:</Tittle>
-        <FeedbackCounter>Good: {good}</FeedbackCounter>
+            <Tittle>Statistics:</Tittle>
+            <StatisticsContainer>
+<FeedbackCounter>Good: {good}</FeedbackCounter>
         <FeedbackCounter>Neutral: {neutral}</FeedbackCounter>
-        <FeedbackCounter>Bad: {bad}</FeedbackCounter>
+            <FeedbackCounter>Bad: {bad}</FeedbackCounter>
+             <FeedbackCounter>Total: {this.countTotalFeedback()}</FeedbackCounter>
+        <FeedbackCounter>
+          Positive feedback: {this.countPositiveFeedbackPercentage()}%
+        </FeedbackCounter>
+            </StatisticsContainer>
+        
       </Container>
     );
      }
--- src/components/Feddback/Feedback.styled.js
+++ src/components/Feddback/Feedback.styled.js
@@ -7,19 +7,37 @@ export const Container = styled.div`
 
 
 export const Tittle = styled.h2`
-  margin: 0 0 10px 0;
+  margin: 0 0 20px 0;
+  font-weight: 800;
 `;
 
+export const ButtonContainer = styled.div`
+display: flex;
+gap: 10px;
+margin-bottom: 20px;
+`;
+
+
 export const Button = styled.button`
-margin: 0 10px 10px 0;
+
 font-size: 18px;
+font-weight: 600;
   border: solid 2px #d9d9d9;
    border-radius: 4px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 
+export const StatisticsContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
+`;
+
+
+
 export const FeedbackCounter = styled.p`
-margin: 0 0 10px 0;
+margin: 0;
 font-size: 18px;
+font-weight: 600;
 `;
